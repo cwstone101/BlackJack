03cdd3a0b3c12d455a3e976f22c0bd534ec12cd1
cwstone101 <102690357+cwstone101@users.noreply.github.com>
2022-05-03 10:53:29 -0400

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,34 @@
+
+/*
+array for deck of cards 
+
+
+button mechanics for instructions to appear
+    onclick to throw up a prompt/alert with info in it or queryselector with an event listen and innerHTML fill of the words into a div
+
+button mechanics for win conditions to appear 
+    onlick to throw up a prompt/alert with info in it or a queryselector with an event listen and innerHTML fill of the words into a div
+
+mechanics for shuffled deck
+    math.random function to mix up the cards that are in the array so each game has a fresh randomized deck
+
+
+must take point totals of cards along with points of face cards and ace special values
+    function to convert selected cards to values for scoring
+
+mechanics for figuring out score of the hands
+    function to score the scores created by the hands at their current state
+
+mechanics for comparing board state of hands to decide victory/loss, stay or hit, etc...
+    if or while statement to evaluate hand score to see if game is done, who won, if there's still ability to hit etc...
+
+after logic to score hands mechanism to allow player to hit or Stay 
+    if or while statement to evaluate hand score to see if game is done, who won, if there's still ability to hit etc...
+
+button mechanic for playing again either for game to keep going until player or dealer wins 5 or to start a new game
+    onclick or queryselector event listen that restarts everything to a new game state
+
+scoring to decide winning the game is a best of 5 games or first to 5
+    while loop comparing scores of dealer and player...first to 3 wins declared winner or if 5 reached winner declared  
+*/
+
