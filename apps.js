08c737dd1a3fb4dc7dd9ad9a8118e4df37d997e7
cwstone101 <102690357+cwstone101@users.noreply.github.com>
2022-05-04 15:11:54 -0400

    Add files via upload
    Deck creation js another version

--- apps.js
+++ apps.js
@@ -1,4 +1,36 @@
 
+
+
+function createDeck(){
+
+    let suits = ['H', 'C', 'D', 'S'];
+    let ranks = ['2','3','4','5','6','7','8','9','T','J','Q', 'K', 'A'];
+    let deck = [];
+    
+    for (let suitCounter = 0; suitCounter < 4; suitCounter++){
+      for (let rankCounter = 0; rankCounter < 13; rankCounter++){
+        //console.log(ranks[rankCounter] + suits[suitCounter]);
+        deck.push(ranks[rankCounter] + suits[suitCounter]);
+      }
+      
+    }return deck;
+    }
+    
+    function shuffleDeck(deck){
+    for(let i = 0; i < 52; i++){
+      let tempCard = deck[i];
+      let randomIndex = Math.floor(Math.random() * 52);
+      deck[i] = deck[randomIndex];
+      deck[randomIndex] = tempCard;
+      }
+    }
+    
+    let testDeck = createDeck();
+    
+    shuffleDeck(testDeck);
+    console.log(testDeck);
+    
+
 /*
 array for deck of cards 
 
