fc01ad16a58ed60422d5ee77c22e4710f25abaa2
cwstone101 <102690357+cwstone101@users.noreply.github.com>
2022-05-04 23:35:30 -0400

    Add files via upload
    Different/more on javascript work

--- apps.js
+++ apps.js
@@ -1,37 +1,162 @@
 
 
 
-function createDeck(){
 
-    let suits = ['H', 'C', 'D', 'S'];
-    let ranks = ['2','3','4','5','6','7','8','9','T','J','Q', 'K', 'A'];
-    let deck = [];
-    
-    for (let suitCounter = 0; suitCounter < 4; suitCounter++){
-      for (let rankCounter = 0; rankCounter < 13; rankCounter++){
-        //console.log(ranks[rankCounter] + suits[suitCounter]);
-        deck.push(ranks[rankCounter] + suits[suitCounter]);
-      }
-      
-    }return deck;
+let dealerSum = 0;
+let yourSum = 0;
+
+let dealerAceCount = 0;
+let yourAceCount = 0;
+
+let deck;
+
+let canHit = true; 
+
+window.onload = function(){
+    buildDeck();
+    shuffleDeck();
+    startGame();
+}
+
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5","6","7","8","9","10","J","Q", "K" ];
+    let types = ["C", "D","H","S"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for(let j = 0; j < values.length; j++){
+            deck.push(values[j] + "-" + types[i]);
+        }
     }
-    
-    function shuffleDeck(deck){
-    for(let i = 0; i < 52; i++){
-      let tempCard = deck[i];
-      let randomIndex = Math.floor(Math.random() * 52);
-      deck[i] = deck[randomIndex];
-      deck[randomIndex] = tempCard;
-      }
+    // console.log(deck);
+}
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length); 
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
     }
-    
-    let testDeck = createDeck();
-    
-    shuffleDeck(testDeck);
-    console.log(testDeck);
-    
+    console.log(deck);
+}
+
+function startGame(){
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden)
+    console.log(hidden);
+    console.log(dealerSum);
+    while (dealerSum < 17){
+
+       let cardImg = document.createElement("img");
+       let card = deck.pop();
+       cardImg.src = "./cards/" + card + ".png";
+         console.log(card);
+       dealerSum += getValue(card);
+       dealerAceCount += checkAce(card);
+       document.getElementById("dealerCardAreaID").append(cardImg);
+
+    }
+    console.log(dealerSum);
+
+    for (let i = 0; i < 2; i++){
+        let cardImg = document.createElement("img");
+       let card = deck.pop();
+       cardImg.src = "./cards/" + card + ".png";
+         console.log(card);
+       yourSum += getValue(card);
+       yourAceCount += checkAce(card);
+       document.getElementById("playerCardAreaID").append(cardImg);
+    }
+    console.log(yourSum);
+    document.getElementById("hitMeID").addEventListener("click", hit);
+    document.getElementById("stayID").addEventListener("click", stay);
+
+}
+
+
+function hit(){
+    if (!canHit){
+        return;
+    }
+    let cardImg = document.createElement("img");
+       let card = deck.pop();
+       cardImg.src = "./cards/" + card + ".png";
+         console.log(card);
+       yourSum += getValue(card);
+       yourAceCount += checkAce(card);
+       document.getElementById("playerCardAreaID").append(cardImg);
+
+       if (reduceAce(yourSum, yourAceCount) > 21){
+           canHit = false
+       }
+
+}
+
+function stay(){
+    dealerSum = reduceAce(dealerSum, dealerAceCount);
+    yourSum = reduceAce(yourSum, yourAceCount);
+
+    canHit  = false;
+    document.getElementById("hidden").src = "./cards" + hidden + ".png";
+
+    let message = "";
+    if (yourSum > 21){
+        message = "You Lose!";
+    }
+    else if (dealerSum > 21){
+        message = "You Win!";
+    }
+    else if (yourSum == dealerSum){
+        message = "Tie!!";
+    }
+    else if (yourSum > dealerSum){
+        message = "You Win!";
+
+    }
+    else if (yourSum < dealerSum){
+        message = "You Lose!";
+    }
+
+    document.getElementById("results").innerText = message;
+}
+
+
+
+
+function getValue(card) {
+    let data = card.split("-"); 
+    let value = data[0];
+
+    if (isNaN(value)) {
+        if (value == "A"){
+            return 11;
+        }
+        return 10;
+    }   
+    return parseInt(value);
+}
+
+function checkAce(card) {
+    if (card[0] == "A"){
+        return 1;
+    }
+    return 0;
+}
+
+function reduceAce(playerSum, playerAceCount){
+    while (playerSum > 21 && playerAceCount > 0){
+        playerSum -= 10;
+        playerAceCount -= 1;
+
+    }
+    return playerSum;
+}
+
 
 /*
+
 array for deck of cards 
 
 dialog tag for modals that are similar to alert/prompt    
@@ -63,5 +188,40 @@ button mechanic for playing again either for game to keep going until player or 
 
 scoring to decide winning the game is a best of 5 games or first to 5
     while loop comparing scores of dealer and player...first to 3 wins declared winner or if 5 reached winner declared  
-*/
+
+
+
+
+function createDeck(){
+
+let suits = ['H', 'C', 'D', 'S'];
+let ranks = ['2','3','4','5','6','7','8','9','T','J','Q', 'K', 'A'];
+let deck = [];
+
+for (let suitCounter = 0; suitCounter < 4; suitCounter++){
+  for (let rankCounter = 0; rankCounter < 13; rankCounter++){
+    //console.log(ranks[rankCounter] + suits[suitCounter]);
+    deck.push(ranks[rankCounter] + suits[suitCounter]);
+  }
+  
+}return deck;
+}
+
+function shuffleDeck(deck){
+for(let i = 0; i < 52; i++){
+  let tempCard = deck[i];
+  let randomIndex = Math.floor(Math.random() * 52);
+  deck[i] = deck[randomIndex];
+  deck[randomIndex] = tempCard;
+  }
+}
+
+let testDeck = createDeck();
+
+shuffleDeck(testDeck);
+console.log(testDeck);
+
+
+
+    */
 
